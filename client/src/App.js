d27c35a8c9f0ee5bf65f94d9056ace5b8524c806
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-09-18 23:24:19 -0400

    attempt deploy 2

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,6 @@ import './App.css';
 
 function App() {
 
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    fetch("/api/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
-
-  if (!user) return <Login onLogin={setUser} />;
 
   return (
     <div className="App">
