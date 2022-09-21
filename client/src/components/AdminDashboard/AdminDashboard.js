a054d9edbe996195a850fdf1c053f52ade5c0863
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-09-20 20:01:41 -0400

    Side bar implemented on Admin dash

--- client/src/components/AdminDashboard/AdminDashboard.js
+++ client/src/components/AdminDashboard/AdminDashboard.js
@@ -1,13 +1,43 @@
-import SideBar from "../SideBar/SideBar"
+import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 function Dashboard({user}) {
 
     const welcome = `Welcome ${user.name}`
     return (
-        <>
-            <h1>Admin Dash</h1>
-            {welcome}
-        </>
+        <div>
+        <Sidebar.Pushable as={Segment} style={{minHeight: '100vh'}}>
+            <Sidebar
+                as={Menu}
+                animation='overlay'
+                icon='labeled'
+                inverted
+                vertical
+                visible
+                width='thin'
+            >
+                <Menu.Item as='a'>
+                <Icon name='home' />
+                    Home
+                </Menu.Item>
+                <Menu.Item as='a'>
+                <Icon name='user' />
+                    Users
+                </Menu.Item>
+                <Menu.Item as='a'>
+                <Icon name='book' />
+                    Trainings
+                </Menu.Item>
+            </Sidebar>
+
+            <Sidebar.Pusher>
+                <Segment basic>
+                    <Header as='h3'>Admin Dash</Header>
+                    <Image src='/images/wireframe/paragraph.png' />
+                    {welcome}
+                </Segment>
+            </Sidebar.Pusher>
+        </Sidebar.Pushable>
+        </div>
     )
 }
 
